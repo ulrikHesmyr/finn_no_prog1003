7d148b9ec576052a0a4f737745fbe1f70531f780
mathileg <mathileg@stud.ntnu.no>
2023-03-23 14:00:39 +0100

    Lagt til i const.h

---
+++ Headers/const.h
@@ -0,0 +1,9 @@
+#ifndef __CONST_H
+#define __CONST_H
+
+
+const int   MAXPRIS  = 20000;	///< Maksimal pris på en vare
+const int   MINPRIS  = 0;	///< Minstepris på en vare
+const int   MINALDER = 13;	///< Laveste mulige alder på selger
+
+#endif
