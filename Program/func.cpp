f849e6c9d0ccb3a4f9d5b93d1d6f661a218b9c5d
oliveta <oliveta@stud.ntnu.no>
2023-03-25 15:53:27 +0100

    smaafiks

--- Program/func.cpp
+++ Program/func.cpp
@@ -12,6 +12,11 @@ void skrivMeny() {
 	std::cout << "Velg type handling:" << std::endl
 			  << "\t U - Kunder" << std::endl
 			  << "\t A - Kategori" << std::endl
-			  << "\t T - Kunder" << std::endl
+			  << "\t T - Ting" << std::endl
 			  << "\t K - Kjop" << std::endl;
+<<<<<<< HEAD:Program/func.cpp
+=======
+
+
+>>>>>>> 09f19a8 (smaafiks):Sources/func.cpp
 }
